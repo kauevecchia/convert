0b06f0b2f63a8ba3154e52159ccc92a6df9dc233
Kauê <kauevecchia@hotmail.com>
2024-07-11 18:02:16 -0300

    formatando o total

--- scripts.js
+++ scripts.js
@@ -3,13 +3,13 @@ const USD = 5.45
 const EUR = 5.92
 const GBP = 7.03
 
-
 // Obtendo os elementos do formulário.
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 const footer = document.querySelector("main footer")
 const description = document.getElementById("description")
+const result = document.getElementById("result")
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
@@ -39,7 +39,21 @@ function convertCurrency(amount, price, symbol) {
     try {
         // Exibindo a cotação da moeda selecionada.
         description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
-        
+
+        // Calcula o total
+        let total = amount * price
+
+        // Verifica se o resultado não é um número.
+        if(isNaN(total)) {
+            return alert("Por favor, digite o valor corretamente para converter.")
+        }
+
+        // Formatar o valor total.
+        total = formatCurrencyBRL(total).replace("R$", "")
+
+        // Exibe o resultado total.
+        result.textContent = `${total} Reais`
+
         // Aplica a classe que exibe o footer para mostrar o resultado.
         footer.classList.add("show-result")
 
